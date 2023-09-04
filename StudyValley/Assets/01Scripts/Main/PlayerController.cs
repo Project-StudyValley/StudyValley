03f91d2a53018e64b9503862b3367b65cc9b2520
UBDJ <icek314@gmail.com>
2023-09-04 17:58:39 +0900

    Fix: Animation 수정, 플레이어 타일맵 충돌 수정, PlayerController.cs 수정

--- StudyValley/Assets/01Scripts/Main/PlayerController.cs
+++ StudyValley/Assets/01Scripts/Main/PlayerController.cs
@@ -7,6 +7,11 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D playerRB;
     private Animator playerAnim;
 
+    private GameObject scanObject;
+    private Vector2 vec2;
+
+
+
     [SerializeField]
     private float speed;
 
@@ -17,10 +22,17 @@ public class PlayerController : MonoBehaviour
         playerAnim = GetComponent<Animator>();
     }
 
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        playerRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
+        playerRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * 0.005f;
 
         playerAnim.SetFloat("moveX", playerRB.velocity.x);
         playerAnim.SetFloat("moveY", playerRB.velocity.y);
@@ -30,5 +42,19 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
             playerAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
         }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+/*        //Ray(조사액션)
+        Debug.DrawRay(playerRB.position, vec2 * 0.7f, new Color(0, 1, 0));
+        *//*Object라는 레이어를 만들어서 Objcect라는 애만 조사*//*
+        RaycastHit2D rayHit = Physics2D.Raycast(playerRB.position, vec2, 0.7f, LayerMask.GetMask("Object"));
+
+        if (rayHit.collider != null) scanObject = rayHit.collider.gameObject;
+        else scanObject = null;*/
     }
 }
+

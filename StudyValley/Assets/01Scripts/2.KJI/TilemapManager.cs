6cf31bae8c43fd3c2725ed12c0a1fdc717f7635f
Kwonjeongin <ad160515@gmail.com>
2023-09-07 10:47:30 +0900

    젖은 땅에 시간이 지나면 마른 땅으로 돌아오는 코드 추가

--- StudyValley/Assets/01Scripts/2.KJI/TilemapManager.cs
+++ StudyValley/Assets/01Scripts/2.KJI/TilemapManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
 
 public class TilemapManager : MonoBehaviour
 {
@@ -11,11 +13,23 @@ public class TilemapManager : MonoBehaviour
     public Tilemap Seedtilemap;
     public Tile[] SeedTile;
     public Tile[] WetTile;
+    private Tilemapstate currentState;
+
+    public enum Tilemapstate
+    {
+        Dry,
+        Wet
+
+    }
+    private void Start()
+    {
+
+    }
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))//Space바(씨앗)를 이용해 작물을 심을 수 있다.
         {
             if (Seedtilemap.GetTile(grid.WorldToCell(transform.position)) == SeedTile[0])
             {
@@ -43,18 +57,35 @@ public class TilemapManager : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftAlt))
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))//Alt키(물뿌리개) 입력시 물에 젖은 땅이 생긴다.
         {
             if (Wettilemap.GetTile(grid.WorldToCell(transform.position)) == WetTile[0])
             {
                 Wettilemap.SetTile(grid.WorldToCell(transform.position), WetTile[1]);
-                print("1");
+
+                StartCoroutine(StateTransitionCoroutine(transform.position));
             }
 
             else
             {
-                Wettilemap.SetTile(grid.WorldToCell(transform.position), WetTile[0]);
+ 
+
             }
         }
+                
     }
+    //마른 땅에 물을 줘서 젖은 땅으로 바꾼다
+    //젖은 땅은 30초가 지나면 마른 땅으로 돌아온다.
+
+    IEnumerator StateTransitionCoroutine(Vector3 position)
+    {
+
+        //3초 기다린다.
+        yield return new WaitForSeconds(3.0f);
+
+        //젖은땅이 마른땅이 된다.
+        Wettilemap.SetTile(grid.WorldToCell(position), WetTile[0]);
+    }
+
 }
